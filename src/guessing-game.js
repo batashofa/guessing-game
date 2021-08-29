e0db9dfeaafa90c5af091c3dd70765a5264e6925
batashofa <batashofa@gmail.com>
2021-08-30 00:23:01 +0300

    feat: added quessing-game solution

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,25 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+        this.result = null;
+        this.min = null;
+        this.max = null;
+    }
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
 
     guess() {
-
+        return this.result = Math.round((this.max + this.min) / 2);
     }
 
     lower() {
-
+        this.max = this.result;
     }
 
     greater() {
-
+        this.min = this.result
     }
 }
 
